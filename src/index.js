b6e620399a4daf20ccad129d4546ba79e449cdf0
Sahil Bhatia <sahil@joshsoftware.com>
2020-04-19 12:37:45 +0530

    read user details from cache

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
+import { toIdValue } from "apollo-utilities";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "@apollo/react-hooks";
 
 import UsersList from "./users/UsersList.js";
 import * as serviceWorker from "./serviceWorker";
 
+const cache = new InMemoryCache({
+  cacheRedirects: {
+    Query: {
+      test_users_by_pk: (_, args) =>
+        toIdValue(
+          cache.config.dataIdFromObject({
+            __typename: "test_users",
+            id: args.id,
+          })
+        ),
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:8080/v1/graphql",
+  cache,
 });
 
 ReactDOM.render(
